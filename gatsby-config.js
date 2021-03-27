de6139fe2ec630874267db8f3068dc1dd4be837d
Dmitry <Avulsed.jb@gmail.com>
2021-03-28 00:06:03 +0200

    ym enable

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,24 +49,24 @@ module.exports = {
     },
     `gatsby-plugin-preact`,
     `gatsby-plugin-netlify`,
-    // {
-    //   resolve: `gatsby-plugin-yandex-metrika`,
-    //   options: {
-    //     // The ID of yandex metrika.
-    //     trackingId: 54720262,
-    //     // Enabled a webvisor. The default value is `false`.
-    //     webvisor: true,
-    //     // Enables tracking a hash in URL. The default value is `false`.
-    //     trackHash: true,
-    //     // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
-    //     // and `true` means after the body (faster loading, less hits). The default value is `false`.
-    //     afterBody: true,
-    //     // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
-    //     // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
-    //     defer: false,
-    //     useCDN: true,
-    //   }, d
-    // },
+    {
+      resolve: `gatsby-plugin-yandex-metrika`,
+      options: {
+        // The ID of yandex metrika.
+        trackingId: 54720262,
+        // Enabled a webvisor. The default value is `false`.
+        webvisor: true,
+        // Enables tracking a hash in URL. The default value is `false`.
+        trackHash: true,
+        // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
+        // and `true` means after the body (faster loading, less hits). The default value is `false`.
+        afterBody: true,
+        // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
+        // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
+        defer: false,
+        useCDN: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-breadcrumb`,
       options: {
