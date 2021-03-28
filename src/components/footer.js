46fd59df1b55b43eb041c262b66f2e4e97d01df5
Dmitry <Avulsed.jb@gmail.com>
2021-03-28 15:13:13 +0200

    fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import FooterIllustration from "../images/footer-illustration.svg";
 const Footer = () => {
     return(
         <footer className="footer section">
-            <img width="100%" height="100%"  src={FooterIllustration} className="footer-illustration" data-aos="fade-left" alt=""/>
+            <img src={FooterIllustration} className="footer-illustration" data-aos="fade-left" alt=""/>
             <div  id="footer" className="footer-title">
                 <h2 className="footer-title__title title-h2-white">Готовы обсудить ваш проект или идею?</h2>
                 <h3 className="footer-title__subtitle title-h3-white">Свяжитесь со мной и мы обсудим детально</h3>
