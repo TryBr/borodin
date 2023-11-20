3f7fae4baac8e5556d93e76cce4c03ab57e77361
trybr <avulsed.jb@gmail.com>
2023-11-21 01:25:56 +0500

    link updated

--- src/components/project.js
+++ src/components/project.js
@@ -1,51 +1,51 @@
-import React from "react";
+import React from "react"
 
-const Project = (
-  {
-    content: {
-        title,
-        link,
-        image: {
-            file: {
-                url
-            }
-        },
-        description: {
-            description
-        }
-    }
-  }
-) => {
+const Project = ({
+  content: {
+    title,
+    link,
+    image: {
+      file: { url },
+    },
+    description: { description },
+  },
+}) => {
   return (
     <div className="col-lg-6">
-        <div className="portfolio-item">
-            <div className="portfolio-item-preview">
-                {
-                    url ? <img className="portfolio-item-preview__img" src={url} alt={title} /> : ''
-                }
-            </div>
-            <div className="portfolio-info">
-                <div className="portfolio-info__title title-h4">
-                    { title }
-                </div>
-                <div className="portfolio-info__separator"></div>
-                <div className="portfolio-info__description">
-                    { description }
-                </div>
-                <div className="portfolio-info-btn btn-orange">
-                    <a href={link} target="_blank" rel="noreferrer" className="portfolio-info-btn__link">
-                        <div className="portfolio-info-btn__title">
-                        Посмотреть
-                        </div>
-                        <div className="portfolio-info-btn__icon"></div>
-                    </a>
-                </div>
-            </div>
+      <div className="portfolio-item">
+        <div className="portfolio-item-preview">
+          {url ? (
+            <img
+              className="portfolio-item-preview__img"
+              src={url}
+              alt={title}
+            />
+          ) : (
+            ""
+          )}
         </div>
+        <div className="portfolio-info">
+          <div className="portfolio-info__title title-h4">{title}</div>
+          <div className="portfolio-info__separator"></div>
+          <div className="portfolio-info__description">{description}</div>
+
+          {link && (
+            <div className="portfolio-info-btn btn-orange">
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                className="portfolio-info-btn__link"
+              >
+                <div className="portfolio-info-btn__title">Посмотреть</div>
+                <div className="portfolio-info-btn__icon"></div>
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
-
-
 
 export default Project
--- src/components/service.js
+++ src/components/service.js
@@ -1,66 +1,49 @@
-import React from "react";
-import { Link } from "react-scroll";
+import React from "react"
+import { Link } from "react-scroll"
 
-const Service = (
-  {
-    content: {
-      title,
-      description: {
-        description
-      },
-      checkList,
-      price,
-      days,
-      icon: {
-        file: {
-          url
-        }
-      }
-    }
-  }
-) => {
+const Service = ({
+  content: {
+    title,
+    description: { description },
+    checkList,
+    price,
+    days,
+    icon: {
+      file: { url },
+    },
+  },
+}) => {
   return (
-
     <div className="col-lg-4">
-        <div className="services-item">
-            <div className="services-item__icon">
-              {
-                url ? <img width="180" height="180"  src={url} alt={title} /> : ''
-              }
-            </div>
-            <div className="services-item__title">
-              {title}
-            </div>
-            <div className="services-item__separator"></div>
-            <div className="services-item__description">
-              {description}
-            </div>
-            <ul className="services-item-list">
-              {
-                checkList.map(( listItem, i ) => (
-                  <li key={i} className="services-item-list__item">{listItem}</li>
-                ))
-              }
-            </ul>
-            <div className="services-item-price">
+      <div className="services-item">
+        <div className="services-item__icon">
+          {url ? <img width="180" height="180" src={url} alt={title} /> : ""}
+        </div>
+        <div className="services-item__title">{title}</div>
+        <div className="services-item__separator"></div>
+        <div className="services-item__description">{description}</div>
+        <ul className="services-item-list">
+          {checkList.map((listItem, i) => (
+            <li key={i} className="services-item-list__item">
+              {listItem}
+            </li>
+          ))}
+        </ul>
+        {/* <div className="services-item-price">
                 <span className="services-item-price__value">От {price} ₽</span>
                 <span className="services-item-price__separator"> / </span>
                 <span className="services-item-price__time">{days}</span>
                 <span className="services-item-price__days">дней</span>
-            </div>
-            <div className="services-item-btn btn-orange">
-                <Link to="footer" className="services-item-btn__link">
-                    <div className="services-item-btn__title">
-                      Оставить заявку
-                    </div>
-                    <div className="services-item-btn__icon"></div>
-                </Link>
-            </div>
+            </div> */}
+        <div className="services-item-btn btn-orange">
+          <Link to="footer" className="services-item-btn__link">
+            <div className="services-item-btn__title">Оставить заявку</div>
+            <div className="services-item-btn__icon"></div>
+          </Link>
         </div>
-    </div>   
+      </div>
+    </div>
   )
 }
-
-
 
 export default Service
